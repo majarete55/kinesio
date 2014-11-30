9cd5b1b0308c4692ad3d707b24b564fe3c3e2c2a
majarete55 <jvaras05@hotmail.com>
2014-11-30 20:23:55 -0300

    cambio de clave
    cambio de clave

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    mysql_connect("localhost", "root", "261089") or die(mysql_error()) ;
+    mysql_connect("localhost", "root", "1234") or die(mysql_error()) ;
     mysql_select_db("kinesiobd") or die(mysql_error()) ;
 mysql_query("SET NAMES 'utf-8'");
 
